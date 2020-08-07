658fb770d968ac18b3b5b9c3c5971a2184d119d5
gabyballester <gaby.ballester@gmail.com>
2020-08-07 16:13:42 +0200

    Componente header

--- src/App.js
+++ src/App.js
@@ -1,8 +1,27 @@
-import React from 'react';
+import React from "react";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+// Redux
+import { Provider } from "react-redux";
+import store from "./store";
 
 function App() {
   return (
-   <p>Crud con redux</p>
+    <Router>
+      <Provider store={store}>
+        <Header />
+        <div className="container">
+          <Switch>
+            <Route exact path="/" component={Productos} />
+            <Route exact path="/productos/nuevo" component={NuevoProducto} />
+            <Route
+              exact
+              path="/productos/editar/:id"
+              component={EditarProducto}
+            />
+          </Switch>
+        </div>
+      </Provider>
+    </Router>
   );
 }
 
---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Header = () => {
+    return ( 
+        <nav className="navbar navbar-expand-lg
+        navbar-dark bg-primary justify-content-between">
+            <h1>CRUD - React, Redux, Hooks, Rest API y Axios</h1>
+        </nav>
+     );
+}
+
+export default Header; 
