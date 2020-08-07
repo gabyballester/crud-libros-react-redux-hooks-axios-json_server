56825d01a643d87ca67b849c73a0e26cf2d0053b
gabyballester <gaby.ballester@gmail.com>
2020-08-07 16:43:45 +0200

    Agrego al header link para llevar a Productos y botón para agregar nuevo

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,12 +1,28 @@
-import React from 'react';
+import React from "react";
+import { Link } from "react-router-dom";
 
 const Header = () => {
-    return ( 
-        <nav className="navbar navbar-expand-lg
-        navbar-dark bg-primary justify-content-between">
-            <h1>CRUD - React, Redux, Hooks, Rest API y Axios</h1>
-        </nav>
-     );
-}
+  return (
+    <nav
+      className="navbar navbar-expand-lg
+        navbar-dark bg-primary justify-content-between"
+    >
+      <div className="container">
+        <h3>
+          <Link to={"/"} className="text-light">
+            CRUD - React, Redux, Hooks, Rest API y Axios
+          </Link>
+        </h3>
+        <Link
+          to={"/productos/nuevo"}
+          className="btn btn-danger
+        nuevo-post d-block d-md-inline-block"
+        >
+          Agregar Producto &#43; {/* + */}
+        </Link>
+      </div>
+    </nav>
+  );
+};
 
-export default Header; 
+export default Header;
