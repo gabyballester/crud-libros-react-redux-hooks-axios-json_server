c8befeab72f68e6a05550a93adde25c7ae26b983
gabyballester <gaby.ballester@gmail.com>
2020-08-07 17:43:02 +0200

    Agrego types para validación y agregar producto

--- src/components/NuevoProducto.jsx
+++ src/components/NuevoProducto.jsx
@@ -1,7 +1,68 @@
-import React from 'react';
+import React, { useState } from "react";
 
 const NuevoProducto = () => {
-    return ( <p>Nuevo..</p> );
-}
- 
+  //state inicial vacío
+  const [nombre, guardarNombre] = useState("");
+  const [precio, guardarPrecio] = useState("");
+
+  // Agregar nuevo producto
+  const submitNuevoProducto = (e) => {
+    e.preventDefault();
+
+    // Validar formulario
+    if (nombre.trim() === "" || precio.trim() === "") {
+      console.log("Falta nombre o precio");
+      return null;
+    }
+  };
+
+  // Si pasa la validación
+
+  // Crear el nuevo producto
+
+  // redireccionar
+
+  return (
+    <div className="row justify-content-center mt-5">
+      <div className="col-md-8">
+        <div className="card">
+          <div className="card-body">
+            <h2 className="text-center mb-4 font-weight-bold ">
+              Agregar Nuevo Libro
+            </h2>
+            <form onSubmit={submitNuevoProducto}>
+              <div className="form-group">
+                <label>Nombre Libro</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Nombre Libro"
+                  value={nombre}
+                  onChange={(e) => guardarNombre(e.target.value)}
+                />
+              </div>
+              <div className="form-group">
+                <label>Precio Libro</label>
+                <input
+                  type="text"
+                  className="form-control"
+                  placeholder="Precio Libro"
+                  value={precio}
+                  onChange={(e) => guardarPrecio(e.target.value)}
+                />
+              </div>
+              <button
+                type="submit"
+                className="btn btn-primary font-weight-bold text-uppercase d-block w-100"
+              >
+                Agregar
+              </button>
+            </form>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
 export default NuevoProducto;
